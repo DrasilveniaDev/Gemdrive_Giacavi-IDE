e73b1688677d5c92f8e8af3f6dfa0285515edf8c
Dalsania <81883338+DalsaniaCode@users.noreply.github.com>
2026-02-02 22:03:24 -0500

    Create compile_for_Windows.bat

---
+++ compile_for_Windows.bat
@@ -0,0 +1,5 @@
+echo "Warning! You need to have a MinGW or a GNU compiler for that. If you don't have it, you need to install it"
+gcc -c unicode_kit.c -o unicode_kit.o
+g++ main.cpp -o "Gemdrive Giacavi 1.0a" -O2 -lSDL3
+rem this has to many lacks of files for various reasons, i hope more soon add necesary resources.
+pause

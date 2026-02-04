25a2ad45252fb482dc2f09b49f86351f653c4e54
Dalsania <81883338+DalsaniaCode@users.noreply.github.com>
2026-02-04 16:08:17 -0500

    rename folder and include new Source Module

--- compile_Windows_MinGW.bat
+++ compile_Windows_MinGW.bat
@@ -5,17 +5,18 @@ echo.
 echo if your compiler doesn't appear above, you cannot compile it.
 pause
 
-gcc -c "Common Source Modules/unicode_kit.c" -o unicode_kit.o
-if not exist "Common Source Modules/LCT_decodeC.c" (
+gcc -c common_source_modules/unicode_kit.c -o unicode_kit.o
+gcc -c common_source_modules/styleButtonFlow.c -o styleButtonFlow.o
+if not exist common_source_modules/LCT_decodeC.c (
   echo LCT decoder it's not yet public for everyone
   echo So the header was not compiled
   pause
   exit
 )
-gcc -c "Common Source Modules/LCT_decodeC.c" -o LCT_decodeC.o
+gcc -c common_source_modules/LCT_decodeC.c -o LCT_decodeC.o
 
 if exist "unicode_kit.o" if exist "LCT_decodeC.o" (
-  g++ main.cpp unicode_kit.o LCT_decodeC.o -o "Gemdrive Giacavi 1.0a.exe" -O2 -lSDL3 -I"Common Source Modules"
+  g++ main.cpp unicode_kit.o styleButtonFlow.o LCT_decodeC.o -o "Gemdrive Giacavi 1.0a.exe" -O2 -lSDL3 -Icommon_source_modules
 ) else (
   echo Application Compilation failed
 )

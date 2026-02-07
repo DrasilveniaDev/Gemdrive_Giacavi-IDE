4b03eb2b025c54f260f9df06164aecc51cc56425
Drasilvenia 8 <81883338+DrasilveniaDev@users.noreply.github.com>
2026-02-07 11:20:18 -0500

    Update compile_Windows_MinGW.bat

--- compile_Windows_MinGW.bat
+++ compile_Windows_MinGW.bat
@@ -7,6 +7,7 @@ pause
 
 gcc -c common_source_modules/unicode_kit.c -o unicode_kit.o
 gcc -c common_source_modules/styleButtonFlow.c -o styleButtonFlow.o
+gcc -c common_source_modules/codeTokenType.c -o codeTokenType.o
 if not exist common_source_modules/LCT_decodeC.c (
   echo LCT decoder it's not yet public for everyone
   echo So the header was not compiled
@@ -16,7 +17,7 @@ if not exist common_source_modules/LCT_decodeC.c (
 gcc -c common_source_modules/LCT_decodeC.c -o LCT_decodeC.o
 
 if exist "unicode_kit.o" if exist "LCT_decodeC.o" (
-  g++ main.cpp unicode_kit.o styleButtonFlow.o LCT_decodeC.o -o "Gemdrive Giacavi 1.0a.exe" -O2 -lSDL3 -Icommon_source_modules
+  g++ main.cpp unicode_kit.o styleButtonFlow.o codeTokenType.o LCT_decodeC.o -o "Gemdrive Giacavi 1.0a.exe" -O2 -lSDL3 -Icommon_source_modules
 ) else (
   echo Application Compilation failed
 )
